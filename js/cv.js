ba019a7a8fc70ed87f04d006237a604f5cff7c84
celine lienard <ce.lienard@gmail.com>
2017-01-22 16:57:18 +0100

    First commit

---
+++ js/cv.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Initialize and Configure Scroll Reveal Animation
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+
+
+})(jQuery); // End of use strict
